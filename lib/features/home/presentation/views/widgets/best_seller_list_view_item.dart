32504fcea6a9cdb4b5e615b91fb22b5f6c5dbd86
Youssef <ys1465@fayoum.edu.eg>
2025-11-29 18:54:28 +0200

    replace get package with go router package

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,80 +1,85 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/core/utils/constants.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          margin: const EdgeInsets.only(top: 16),
-          width: MediaQuery.of(context).size.width * 0.22,
-          child: AspectRatio(
-            aspectRatio: 2.7 / 4,
-            child: Container(
-              margin: const EdgeInsets.only(right: 16),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.red,
-                image: const DecorationImage(
-                  image: AssetImage(AssetsData.test),
-                  fit: BoxFit.fill,
+    return GestureDetector(
+      onTap: () => GoRouter.of(context).push(AppRouter.kBookDetailsView),
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 16),
+            width: MediaQuery.of(context).size.width * 0.22,
+            child: AspectRatio(
+              aspectRatio: 2.7 / 4,
+              child: Container(
+                margin: const EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.test),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        const SizedBox(width: 12),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Harry Potter and the Goblet of Fire',
-                style: Styles.textStyle20.copyWith(fontFamily: kSectraFont),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              const SizedBox(height: 4),
-              Text('J.K. Rowling', style: Styles.textStyle16),
-              const SizedBox(height: 8),
-              Row(
-                children: [
-                  Text('19.99 €', style: Styles.textStyle20),
-                  const Spacer(),
-                  const Icon(Icons.star, color: Colors.yellow, size: 24),
-                  const SizedBox(width: 4),
-                  // Text(
-                  //   '4.8',
-                  //   style: Styles.textStyle16.copyWith(
-                  //     fontWeight: FontWeight.bold,
-                  //     color: Colors.white,
-                  //   ),
-                  // ),
-                  // const SizedBox(width: 4),
-                  // Text('(2390)', style: Styles.textStyle16),
-                  Text.rich(
-                    TextSpan(
-                      text: '4.8 ',
-                      style: Styles.textStyle16.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+          const SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle20.copyWith(fontFamily: kSectraFont),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 4),
+                Text('J.K. Rowling', style: Styles.textStyle16),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    Text('19.99 €', style: Styles.textStyle20),
+                    const Spacer(),
+                    const Icon(Icons.star, color: Colors.yellow, size: 24),
+                    const SizedBox(width: 4),
+                    // Text(
+                    //   '4.8',
+                    //   style: Styles.textStyle16.copyWith(
+                    //     fontWeight: FontWeight.bold,
+                    //     color: Colors.white,
+                    //   ),
+                    // ),
+                    // const SizedBox(width: 4),
+                    // Text('(2390)', style: Styles.textStyle16),
+                    Text.rich(
+                      TextSpan(
+                        text: '4.8 ',
+                        style: Styles.textStyle16.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        children: [
+                          TextSpan(text: '(2390)', style: Styles.textStyle16),
+                        ],
                       ),
-                      children: [
-                        TextSpan(text: '(2390)', style: Styles.textStyle16),
-                      ],
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,9 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -30,7 +31,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
     animationController.dispose();
     super.dispose();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -57,11 +58,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 2), () {
-      Get.to(
-        () => const HomeView(),
-        transition: Transition.fadeIn,
-        duration: const Duration(seconds: 1),
-      );
+      if (!mounted) return;
+      context.go(AppRouter.kHomeView);
     });
   }
 }
